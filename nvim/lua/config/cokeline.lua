1e1895e6bbddd7eae2354063d18b5a38dd08e6ac
Arne Roomann-Kurrik <kurrik@gmail.com>
2025-05-04 13:26:07 -0700

    refactor(nvim/lua/config): consolidate cokeline and theme configuration
    - Refactored nvim/lua/config/cokeline.lua to include conditional formatting and additional styling based on buffer focus.
    - Added color definitions for the gruvbox light theme in nvim/lua/config/theme.lua.

--- nvim/lua/config/cokeline.lua
+++ nvim/lua/config/cokeline.lua
@@ -1,21 +1,51 @@
--- nvim-cokeline minimal, working configuration
+local colors = require('config.theme').colors
+
 require('cokeline').setup({
   show_if_buffers_are_at_least = 1,
   components = {
     {
-      text = function(buffer) return ' ' .. buffer.index end,
+      text = function(buffer)
+        return buffer.is_focused and ' ' or ' '
+      end,
+      fg = function(buffer)
+        return buffer.is_focused and colors.white or colors.darkgray
+      end,
+      bg = function(buffer)
+        return buffer.is_focused and colors.darkgray or colors.gray
+      end,
     },
     {
-      text = function(buffer) return ' ' .. buffer.filename .. ' ' end,
+      text = function(buffer)
+        local name = buffer.filename
+        return ' ' .. name .. ' '
+      end,
+      style = function(buffer)
+        return buffer.is_focused and 'bold' or nil
+      end,
+      fg = function(buffer)
+        return buffer.is_focused and colors.white or colors.darkgray
+      end,
+      bg = function(buffer)
+        return buffer.is_focused and colors.darkgray or colors.gray
+      end,
     },
     {
       text = '󰅖',
       on_click = function(_, _, _, _, buffer)
         buffer:delete()
-      end
+      end,
+      fg = function(buffer)
+        return buffer.is_focused and colors.white or colors.darkgray
+      end,
+      bg = function(buffer)
+        return buffer.is_focused and colors.darkgray or colors.gray
+      end,
     },
     {
       text = ' ',
+      bg = function(buffer)
+        return buffer.is_focused and colors.darkgray or colors.gray
+      end,
     }
   },
 })
--- nvim/lua/config/theme.lua
+++ nvim/lua/config/theme.lua
@@ -1,6 +1,13 @@
 -- Theme utility for config
 local M = {}
 
+-- These match gruvbox light in https://github.com/nvim-lualine/lualine.nvim/blob/master/lua/lualine/themes/gruvbox_light.lua
+M.colors = {
+  darkgray = '#7c6f64',
+  gray = '#d5c4a1',
+  white = '#f9f5d7',
+}
+
 function M.get_lualine_theme()
   local theme = vim.g.colors_name or 'catppuccin'
   if theme == 'gruvbox' then
